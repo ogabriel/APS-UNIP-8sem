c06bfe96ab566dc57320af0733fcf1c7ebcda48f
ogabriel <gabrieloliver8991@gmail.com>
2020-09-19 19:08:19 -0300

    add postgis extension

---
+++ db/migrations/20200919220732-add-postgis-extension.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query('CREATE EXTENSION postgis;');
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize.query('DROP EXTENSION postgis;');
+  },
+};
