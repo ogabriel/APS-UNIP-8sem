03be98bcb2299dba8db05e9b384ded5aa946d3e6
Breno Lipi <brenorlipi@hotmail.com>
2020-09-23 15:40:09 -0300

    adding models

---
+++ app/models/recycling_station.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const { DataTypes } = require("sequelize/types");
+const { sequelize } = require(".");
+
+module.exports = (sequelize, DataTypes) => {
+    const RecyclingStation = sequelize.define(
+        'RecyclingStation',
+        {
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                },
+            },
+            localization: {
+                type: DataTypes.GEOMETRY('POINT'),
+                allowNull: false,
+            },
+            plastic_kg: {
+                type: DataTypes.FLOAT,
+            },
+            metal_kg: {
+                type: DataTypes.FLOAT,
+            },
+            glass_kg: {
+                type: DataTypes.FLOAT,
+            },
+            paper_kg: {
+                type: DataTypes.FLOAT,
+            },
+            electronic_kg: {
+                type: DataTypes.FLOAT,
+            },
+        }
+    );
+    RecyclingStation.associate = function (models) {
+        RecyclingStation.belongsTo(models.User);
+    };
+
+    return RecyclingStation;
+}
---
+++ app/models/user.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { DataTypes } = require("sequelize/types");
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(
+        'User', 
+    {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+            },
+        },
+        encrypted_password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+    User.associate = function (models) {
+        User.hasMany(models.RecyclingStation);
+    }
+
+    return User;
+}
