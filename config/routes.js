c70ad53eb9ea94989b63546029e696409d7747b6
Breno Lipi <brenorlipi@hotmail.com>
2020-09-23 18:44:43 -0300

    included controller routes

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,10 @@
 const Router = require('express').Router();
 
 // require controllers
-
+const UserController = require('../app/controllers/user_controller');
+const RecyclingStationController = require('../app/controllers/recycling_station_controller');
 // add use controllers
+Router.use('/users', UserController);
+Router.use('/recyclingstations', RecyclingStationController);
 
 module.exports = Router;
