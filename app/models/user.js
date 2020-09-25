a8998ad48eb3948a12f9b05b530aab025440cf4b
Breno Lipi <brenorlipi@hotmail.com>
2020-09-25 14:25:58 -0300

    added password encryption w/ bcrypt

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { DataTypes } = require("sequelize/types");
+const bcrypt = require("bcrypt");
 
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define(
@@ -25,6 +26,15 @@ module.exports = (sequelize, DataTypes) => {
             allowNull: false,
         },
     });
+
+    User.beforeCreate(async (user, options) => {
+        return bcrypt.hash(user.encrypted_password, 10).then(hash => {
+            user.encrypted_password = hash;
+        }).catch(err => {
+            throw new Error();
+        });
+      });
+
     User.associate = function (models) {
         User.hasMany(models.RecyclingStation);
     }
