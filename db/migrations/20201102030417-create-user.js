42b0c72a154c6cc693183d3844ec6fc6314b80b7
Breno Lipi <43282619+brlipi@users.noreply.github.com>
2020-11-02 16:31:52 -0300

    User models, migrations and seeders (#5)
    * User models, migrations and seeders
    
    * seeder
    
    * add bcrypt
    
    * modify names
    
    * udpate seed
    
    * commit association
    
    * user thing
    
    Co-authored-by: ogabriel <gabrieloliver8991@gmail.com>

---
+++ db/migrations/20201102030417-create-user.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        unique: true,
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  },
+};
---
+++ db/seeders/20201102030801-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert(
+      'User',
+      [
+        {
+          name: 'John Doe',
+          email: 'john@doe.com',
+          password: 'password',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      [
+        {
+          name: 'Jane Doe',
+          email: 'jane@doe.com',
+          password: 'password',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ]
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(
+      'User',
+      [{ email: ['john@doe.com', 'jane@doe.com'] }],
+      {}
+    );
+  },
+};
