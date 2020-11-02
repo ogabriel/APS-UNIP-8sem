0296f61c6818a1cb77c4e725eb7fb54d10f1991e
ogabriel <gabrieloliver8991@gmail.com>
2020-11-02 17:37:55 -0300

    fix user seed

--- db/seeders/20201102030801-user.js
+++ db/seeders/20201102030801-user.js
@@ -2,29 +2,26 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert(
-      'Users',
-      [
-        {
-          name: 'John Doe',
-          email: 'john@doe.com',
-          password:
-            '$2b$10$ZnLTx/BzinVUYau1G1OAreZ0yyoKkA9XsROiMG1ndiNzMNdvpy4ke',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      [
-        {
-          name: 'Jane Doe',
-          email: 'jane@doe.com',
-          password:
-            '$2b$10$jR2Lp11D4wD1VfubaWBNZ.cOK0CxnDaZI7/pBzvHxGLsqhrOlYo82',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ]
-    );
+    return queryInterface.bulkInsert('Users', [
+      {
+        name: 'John Doe',
+        email: 'john@doe.com',
+        // password
+        password:
+          '$2b$10$ZnLTx/BzinVUYau1G1OAreZ0yyoKkA9XsROiMG1ndiNzMNdvpy4ke',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Jane Doe',
+        email: 'jane@doe.com',
+        // password
+        password:
+          '$2b$10$jR2Lp11D4wD1VfubaWBNZ.cOK0CxnDaZI7/pBzvHxGLsqhrOlYo82',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
