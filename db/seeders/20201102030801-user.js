323e34c13662f3d7d8f78a5899ced6db47a1629b
Breno Lipi <43282619+brlipi@users.noreply.github.com>
2020-11-02 17:54:49 -0300

    Recycling station models, migrations and seeders and fix some other problems (#4)
    * Recycling station models, migrations and seeders
    
    * Update 20201102024110-recycling-station.js
    
    * Correct seeder code added
    
    * Seeders code fixes
    
    * add foreigh key
    
    * readd assoc for user
    
    * add extra line
    
    * redo timestamps to after user
    
    * change queries
    
    * add names to seeder
    
    * use arrows on relations
    
    * add timestamps to user
    
    * correct name
    
    * fix password
    
    * fix user seed
    
    Co-authored-by: ogabriel <gabrieloliver8991@gmail.com>

--- db/seeders/20201102030801-user.js
+++ db/seeders/20201102030801-user.js
@@ -2,32 +2,31 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert(
-      'User',
-      [
-        {
-          name: 'John Doe',
-          email: 'john@doe.com',
-          password: 'password',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      [
-        {
-          name: 'Jane Doe',
-          email: 'jane@doe.com',
-          password: 'password',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ]
-    );
+    return queryInterface.bulkInsert('Users', [
+      {
+        name: 'John Doe',
+        email: 'john@doe.com',
+        // password
+        password:
+          '$2b$10$ZnLTx/BzinVUYau1G1OAreZ0yyoKkA9XsROiMG1ndiNzMNdvpy4ke',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Jane Doe',
+        email: 'jane@doe.com',
+        // password
+        password:
+          '$2b$10$jR2Lp11D4wD1VfubaWBNZ.cOK0CxnDaZI7/pBzvHxGLsqhrOlYo82',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete(
-      'User',
+      'Users',
       [{ email: ['john@doe.com', 'jane@doe.com'] }],
       {}
     );
---
+++ db/seeders/20201102044110-recycling-station.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const usersQuery = await queryInterface.sequelize.query(
+      'SELECT u.id FROM "Users" u ORDER BY u.id LIMIT 2'
+    );
+
+    const users = usersQuery[0];
+
+    return queryInterface.bulkInsert('RecyclingStations', [
+      {
+        name: 'Recicloptero',
+        localization: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(-46.635906 -23.554604)'
+        ),
+        plastic_kg: 200,
+        metal_kg: 350,
+        glass_kg: 700,
+        paper_kg: 1420,
+        electronic_kg: 130,
+        UserId: users[0].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'R3 reciclagem',
+        localization: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(-46.633706 -23.554604)'
+        ),
+        plastic_kg: 1080,
+        metal_kg: 2160,
+        glass_kg: 360,
+        paper_kg: 180,
+        electronic_kg: 720,
+        UserId: users[1].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete(
+      'RecyclingStations',
+      [{ name: ['Recicloptero', 'R3 reciclagem'] }],
+      {}
+    );
+  },
+};
