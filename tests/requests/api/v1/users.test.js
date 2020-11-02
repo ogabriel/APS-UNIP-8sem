c57b204ea72a24ed2ab2ce4ed4adc0573d322fa1
Breno Lipi <brenorlipi@hotmail.com>
2020-11-01 21:33:36 -0300

    prettier fixes

--- tests/requests/api/v1/users.test.js
+++ tests/requests/api/v1/users.test.js
@@ -4,25 +4,25 @@ const app = require('../../../../app');
 const { createUser } = require('../../../factories/user');
 
 describe('GET /users', () => {
-    describe('when there are no users', () => {
-        test('return an empty array', async () => {
-            const response = await request(app).get('/api/v1/users');
+  describe('when there are no users', () => {
+    test('return an empty array', async () => {
+      const response = await request(app).get('/api/v1/users');
 
-            expect(response.statusCode).toBe(200);
-            expect(response.body).toEqual([]);
-        });
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual([]);
     });
+  });
 
-    describe('when there are users created', () => {
-        beforeEach(() => createUser({ name: 'John'}));
+  describe('when there are users created', () => {
+    beforeEach(() => createUser({ name: 'John' }));
 
-        test('return all created users', async () => {
-            const response = await request(app).get('/api/v1/users');
+    test('return all created users', async () => {
+      const response = await request(app).get('/api/v1/users');
 
-            expect(response.statusCode).toBe(200);
-            expect(response.body).not.toEqual([]);
-            expect(response.body.length).toEqual(1);
-            expect(response.body[0].name).toEqual('John');
-        });
+      expect(response.statusCode).toBe(200);
+      expect(response.body).not.toEqual([]);
+      expect(response.body.length).toEqual(1);
+      expect(response.body[0].name).toEqual('John');
     });
+  });
 });
