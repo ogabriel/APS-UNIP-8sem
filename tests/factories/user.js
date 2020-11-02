30813863b041f74f93a4f39c58eef3c8ac430023
Breno Lipi <brenorlipi@hotmail.com>
2020-11-02 01:11:39 -0300

    User factory

---
+++ tests/factories/user.js
@@ -0,0 +1,18 @@
+const faker = require('faker');
+const { User } = require('../../app/models');
+
+const defaultOptions = {
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+};
+
+const userParams = (options) => ({ ...defaultOptions, ...options });
+
+exports.userParams = (options = {}) => userParams(options);
+
+exports.createUser = async (options = {}) =>
+  await User.create(userParams(options));
+
+exports.buildUser = async (options = {}) =>
+  await User.build(userParams(options));
