135ca29a21b742a287f1eae0f76a9247c6a1675c
Breno Lipi <brenorlipi@hotmail.com>
2020-11-01 21:26:49 -0300

    adding test requests from db-dev branch

---
+++ tests/requests/api/v1/stations.test.js
@@ -0,0 +1,62 @@
+const request = require('supertest');
+const app = require('../../../../app');
+
+const { createStation } = require('../../../factories/recycling_station');
+
+describe('GET /recyclingstations', () => {
+  describe('when there are no stations', () => {
+    test('return an empty array', async () => {
+      const response = await request(app).get('/api/v1/recyclingstations');
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual([]);
+    });
+  });
+
+  describe('when there are stations created', () => {
+    beforeEach(() => createStation({}));
+
+    test('return all the created stations', async () => {
+      const response = await request(app).get('/api/v1/recyclingstations');
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).not.toEqual([]);
+      expect(response.body.length).toEqual(1);
+      expect(response.body[0].discovered).toEqual(false);
+    });
+  });
+});
+
+describe('GET /recyclingstations/localization', () => {
+  describe('when there are no stations', () => {
+    test('return an empty array', async () => {
+      const response = await request(app).get('/api/v1/recyclingstations/localization');
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual([]);
+    });
+  });
+
+  describe('when there are stations created', () => {
+    beforeEach(() => createStation({}));
+
+    test('return all the created stations', async () => {
+      const response = await request(app).get('/api/v1/recyclingstations/localization');
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body).not.toEqual([]);
+      expect(response.body.length).toEqual(1);
+      expect(response.body[0].type).toEqual('Feature');
+      expect(response.body[0].properties).not.toEqual({});
+      expect(response.body[0].geometry.type).toEqual('Point');
+      expect(response.body[0].geometry.coordinates[0]).toBeGreaterThanOrEqual(
+        -90
+      );
+      expect(response.body[0].geometry.coordinates[0]).toBeLessThanOrEqual(90);
+      expect(response.body[0].geometry.coordinates[1]).toBeGreaterThanOrEqual(
+        -180
+      );
+      expect(response.body[0].geometry.coordinates[1]).toBeLessThanOrEqual(180);
+    });
+  });
+});
