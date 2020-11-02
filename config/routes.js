fb6e8889c86525fad0d59de25e5fccf6673f3136
Breno Lipi <brenorlipi@hotmail.com>
2020-11-02 01:09:28 -0300

    User controller route

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,8 @@
 const Router = require('express').Router();
 
 // require controllers
-
+const UserController = require('../app/controllers/user_controller');
 // add use controllers
+Router.use('/users', UserController);
 
 module.exports = Router;
