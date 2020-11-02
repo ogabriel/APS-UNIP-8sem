a497d8b7c8b6a3327d5993d367d7927c791426e2
Breno Lipi <43282619+brlipi@users.noreply.github.com>
2020-11-02 00:01:16 -0300

    Update 20201102024110-recycling-station.js

--- db/seeders/20201102024110-recycling-station.js
+++ db/seeders/20201102024110-recycling-station.js
@@ -35,6 +35,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable('RecyclingStations');
   },
 };
