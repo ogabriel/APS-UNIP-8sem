70bd49440f457bc09e525df2b8b91fc89bc2a091
Breno Lipi <brenorlipi@hotmail.com>
2020-11-02 21:52:14 -0300

    Testing

--- app/controllers/user_controller.js
+++ app/controllers/user_controller.js
@@ -3,6 +3,12 @@
 const router = require('express').Router();
 const { User } = require('../models');
 
+router.get('/', function (req, res) {
+  User.findAll().then((data) => {
+    res.json(data);
+  });
+});
+
 router.get('/:id', function (req, res) {
   User.findByPk(req.params.id).then((data) => {
     res.json(data);
