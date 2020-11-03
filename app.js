c0eda9d7b12852ad48a5eb7db007d4eb120a44c6
ogabriel <gabrieloliver8991@gmail.com>
2020-11-02 22:30:31 -0300

    add session config

--- app.js
+++ app.js
@@ -6,10 +6,22 @@ require('dotenv').config();
 // Configure express
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const app = express();
 
 // Configura json
 app.use(express.json());
+
+// Configure session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'aps8',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+>>>>>>> add session config
 
 // Config static files
 app.use(express.static(path.join(__dirname, 'public')));
