abb2866e9a927d1258fe238288a1868f04d9df36
ogabriel <gabrieloliver8991@gmail.com>
2020-11-04 23:44:10 -0300

    tune tests

--- tests/requests/api/v1/user.test.js
+++ tests/requests/api/v1/user.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+// const session = require('supertest-session');
 const app = require('../../../../app');
 
 const { createUser } = require('../../../factories/user');
@@ -30,29 +31,31 @@ describe('GET /users/:id', () => {
 describe('POST /users/login', () => {
   describe('when the user is not created', () => {
     test('return an empty object', async () => {
-      const response = await request(app).post('/api/v1/users/login');
+      const response = await request(app)
+        .post('/api/v1/users/login')
+        .send({ email: 'john', password: 'password' })
+        .set('Accept', 'application/json');
 
       expect(response.statusCode).toBe(404);
-      expect(response.body).toEqual({});
     });
   });
 
   describe('when the user is created', () => {
     test('return the created user', async () => {
-      const user = await createUser({ name: 'John' });
-      const user_id = user.id;
+      const user = await createUser({ name: 'John', password: 'password' });
 
-      const response = await request(app).post('/api/v1/users/login');
+      const response = await request(app)
+        .post('/api/v1/users/login')
+        .send({ email: user.email, password: 'password' })
+        .set('Accept', 'application/json');
 
       expect(response.statusCode).toBe(200);
-      expect(response.body.id).toEqual(user_id);
-      expect(response.body.name).toEqual('John');
     });
   });
 });
 
 describe('DELETE /users/logout', () => {
-  describe('when the user is not created', () => {
+  describe('when the user was never logged', () => {
     test('return an empty object', async () => {
       const response = await request(app).delete('/api/v1/users/logout');
 
@@ -63,14 +66,18 @@ describe('DELETE /users/logout', () => {
 
   describe('when the user is created', () => {
     test('return the created user', async () => {
-      const user = await createUser({ name: 'John' });
-      const user_id = user.id;
+      const user = await createUser({ name: 'John', password: 'password' });
 
-      const response = await request(app).delete('/api/v1/users/logout');
+      const response_login = await request(app)
+        .post('/api/v1/users/login')
+        .send({ email: user.email, password: 'password' })
+        .set('Accept', 'application/json');
 
-      expect(response.statusCode).toBe(200);
-      expect(response.body.id).toEqual(user_id);
-      expect(response.body.name).toEqual('John');
+      expect(response_login.statusCode).toBe(200);
+
+      const response_logout = await request(app).delete('/api/v1/users/logout');
+
+      expect(response_logout.statusCode).toBe(200);
     });
   });
 });
