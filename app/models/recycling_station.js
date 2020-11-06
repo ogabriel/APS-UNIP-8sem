2d250176e9568466329ba5baa24e69c4db3f3dd0
ogabriel <gabrieloliver8991@gmail.com>
2020-11-06 11:40:09 -0300

    add validation to recycling_station materials

--- app/models/recycling_station.js
+++ app/models/recycling_station.js
@@ -1,34 +1,52 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  const RecyclingStation = sequelize.define('RecyclingStation', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
+  const RecyclingStation = sequelize.define(
+    'RecyclingStation',
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      localization: {
+        type: DataTypes.GEOMETRY('POINT'),
+        allowNull: false,
+      },
+      plastic: {
+        type: DataTypes.INTEGER,
+      },
+      metal: {
+        type: DataTypes.INTEGER,
+      },
+      glass: {
+        type: DataTypes.INTEGER,
+      },
+      paper: {
+        type: DataTypes.INTEGER,
+      },
+      electronic: {
+        type: DataTypes.INTEGER,
       },
     },
-    localization: {
-      type: DataTypes.GEOMETRY('POINT'),
-      allowNull: false,
-    },
-    plastic: {
-      type: DataTypes.INTEGER,
-    },
-    metal: {
-      type: DataTypes.INTEGER,
-    },
-    glass: {
-      type: DataTypes.INTEGER,
-    },
-    paper: {
-      type: DataTypes.INTEGER,
-    },
-    electronic: {
-      type: DataTypes.INTEGER,
-    },
-  });
+    {
+      validate: {
+        atLeastOneType: function () {
+          if (
+            isNaN(this.electronic) &&
+            isNaN(this.glass) &&
+            isNaN(this.metal) &&
+            isNaN(this.paper) &&
+            isNaN(this.plastic)
+          ) {
+            throw new Error('Require at least of type os recycling material');
+          }
+        },
+      },
+    }
+  );
 
   RecyclingStation.associate = (models) => {
     RecyclingStation.belongsTo(models.User);
