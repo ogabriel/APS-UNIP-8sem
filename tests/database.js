b1c8ab6ace0f57b25cf65618f4b83765355c33d9
ogabriel <gabrieloliver8991@gmail.com>
2020-11-07 09:42:12 -0300

    comment

--- tests/database.js
+++ tests/database.js
@@ -1,13 +1,13 @@
 const db = require('../app/models');
 
-// beforeEach(async () => {
-//   return await db.sequelize.sync({ force: true });
-// });
-
-afterEach(async () => {
-  const models = Object.values(db.sequelize.models);
-
-  return await models.forEach((tableName) => {
-    tableName.destroy({ truncate: true, cascade: true, logging: null });
-  });
+beforeEach(async () => {
+  return await db.sequelize.sync({ force: true });
 });
+
+// afterEach(async () => {
+//   const models = Object.values(db.sequelize.models);
+
+//   return await models.forEach((tableName) => {
+//     tableName.destroy({ truncate: true, cascade: true, logging: null });
+//   });
+// });
