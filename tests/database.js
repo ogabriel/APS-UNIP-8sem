e494a659779e41be53f8c92653d1e0ed09ac12c8
ogabriel <gabrieloliver8991@gmail.com>
2020-11-07 09:37:48 -0300

    add configs

--- tests/database.js
+++ tests/database.js
@@ -1,5 +1,9 @@
 const db = require('../app/models');
 
+beforeEach(async () => {
+  return await db.sequelize.sync({ force: true });
+});
+
 afterEach(async () => {
   const models = Object.values(db.sequelize.models);
 
