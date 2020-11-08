f1ba14e047958cdb514f64659f0dca288de43908
ogabriel <gabrieloliver8991@gmail.com>
2020-11-08 14:53:31 -0300

    add js to load table

--- public/js/index.js
+++ public/js/index.js
@@ -5,9 +5,36 @@ async function getData() {
   return data;
 }
 
-// Load map with stores
 function loadTable(data) {
-  return data;
+  const tableBody = document
+    .getElementById('table')
+    .getElementsByTagName('tbody')[0];
+
+  if (data.electronic) {
+    tableInsertRow(tableBody, 'Eletrônicos', data.electronic);
+  }
+  if (data.glass) {
+    tableInsertRow(tableBody, 'Vidros', data.glass);
+  }
+  if (data.metal) {
+    tableInsertRow(tableBody, 'Metal', data.metal);
+  }
+  if (data.paper) {
+    tableInsertRow(tableBody, 'Pepel', data.paper);
+  }
+  if (data.plastic) {
+    tableInsertRow(tableBody, 'Plástico', data.plastic);
+  }
+}
+
+function tableInsertRow(tableBody, material, sum) {
+  var newRow = tableBody.insertRow();
+
+  var cellMaterial = newRow.insertCell();
+  cellMaterial.outerHTML = `<th scope="row">${material}</th>`;
+
+  var sumMaterial = newRow.insertCell();
+  sumMaterial.outerHTML = `<td>${sum}</td>`;
 }
 
 getData().then((data) => loadTable(data));
