bfee5dfde18f058c72cfebdba6c82f87c6b22825
Breno Lipi <brenorlipi@hotmail.com>
2020-11-08 17:55:08 -0300

    Script now is able to send an address

--- public/js/geosearchForm.js
+++ public/js/geosearchForm.js
@@ -1,4 +1,16 @@
-const provider = window.geosearch;
+/* Aparentemente as variáveis em var precisam estar em var. Ainda não testei com const.
+Não executar o prettier por enquanto, capaz de mudar tais coisas.*/
+
+var GeoSearchControl = window.GeoSearch.GeoSearchControl;
+var OpenStreetMapProvider = window.GeoSearch.OpenStreetMapProvider;
+
+var provider = new OpenStreetMapProvider();
+
+/*var searchControl = new GeoSearchControl({  Mapa do leaflet preciusa ser instanciado para usar esse treco
+  provider: provider,
+  autoComplete: true, // optional: true|false  - default true
+  autoCompleteDelay: 650, // optional: number      - default 250
+});*/
 
 const form = document.querySelector('form');
 console.log(form);
