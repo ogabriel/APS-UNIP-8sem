4d88b5d1f7d80d52a25a7e7759e6f601d3831c15
Breno Lipi <brenorlipi@hotmail.com>
2020-11-09 17:50:33 -0300

    Nothing worked

--- public/js/geosearchForm.js
+++ public/js/geosearchForm.js
@@ -1,47 +1,16 @@
-/* Aparentemente as variáveis em var precisam estar em var. Ainda não testei com const.
-Não executar o prettier por enquanto, capaz de mudar tais coisas.*/
+var map = L.map('mapid').setView([-23.554, -46.6333], 16);
 
-var GeoSearchControl = window.GeoSearch.GeoSearchControl;
-var OpenStreetMapProvider = window.GeoSearch.OpenStreetMapProvider;
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
 
-var provider = new OpenStreetMapProvider({ params: { countrycodes: 'br' } });
+  var searchControl = L.esri.Geocoding.geosearch().addTo(map);
 
-var searchControl = new GeoSearchControl({
-  provider: provider,
-  style: 'bar',
-  searchLabel: 'Insira a localização da estação',
-  showMarker: true,
-  marker: {
-    icon: new L.Icon.Default(),
-  },
-  autoClose: true,
-  autoComplete: true,
-  autoCompleteDelay: 400, // optional: number      - default 250
-});
+  var results = L.layerGroup().addTo(map);
 
-var map = new L.Map('mapid').setView([-23.554, -46.6333], 16);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
-
-map.addControl(searchControl);
-
-const form = document.querySelector('form');
-const div = form.querySelector('div[class="form-group address"]');
-const input = div.querySelector('input[type="text"]');
-
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-
-  var results = await provider.search({ query: input.value });
-
-  results.forEach(function (result) {
-    delete result.label;
-    delete result.bounds;
-    delete result.raw;
+  searchControl.on('results', function (data) {
+    results.clearLayers();
+    for (var i = data.results.length - 1; i >= 0; i--) {
+      results.addLayer(L.marker(data.results[i].latlng));
+    }
   });
-
-  console.log(results);
-});
