931e0115030dfa0d8ab41ea881adbdd9cd501e72
Jessica Mitiko <jessicamitiko@hotmail.com>
2020-11-09 21:35:51 -0300

    Integrar o html com js pra mostrar as informacoes

--- public/js/estacao_de_reciclagem.js
+++ public/js/estacao_de_reciclagem.js
@@ -1,91 +1,62 @@
 const stationId = window['id'] || undefined;
-const boxesIds = ["#plastic", "#metal", "#glass", "#paper", "#electronic"];
+const boxesIds = ['#plastic', '#metal', '#glass', '#paper', '#electronic'];
 
 async function putMaterialQuantity(data) {
-    const { id, bodyRequest, endpoint } = data;
-    const response = await fetch(`/api/v1/recycling_stations/${id}/${endpoint}`, {
-        method: 'PUT',
-        mode: 'cors',
-        cache: 'no-cache',
-        credentials: 'same-origin',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        referrerPolicy: 'no-referrer',
-        body: JSON.stringify(bodyRequest)
-    });
+  const { id, bodyRequest, endpoint } = data;
+  const response = await fetch(`/api/v1/recycling_stations/${id}/${endpoint}`, {
+    method: 'PUT',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    referrerPolicy: 'no-referrer',
+    body: JSON.stringify(bodyRequest),
+  });
 
-    return response.json();
+  return response.json();
+}
+
+async function getStationData(id) {
+  const rstationRequest = await fetch(`/api/v1/recycling_stations/${id}`);
+  return await rstationRequest.json();
 }
 
 boxesIds.forEach((boxId) => {
-    var button = document.querySelector(`${boxId} button`);
-    button.onclick = () => {
-        var input = document.querySelector(`${boxId} input[name='quantity']`);
-        var value = parseInt(input.value);
-        if (value && stationId) {
-            switch (boxId) {
-                case "#plastic":
-                    putMaterialQuantity({
-                        id: stationId,
-                        bodyRequest: { g: value },
-                        endpoint: 'add_plastic'
-                    })
-                        .then((response) => {
-                            console.log(response);
-                            deposited = true;
-                        })
-                        .catch(console.error);
-                    break;
-                case "#metal":
-                    putMaterialQuantity({
-                        id: stationId,
-                        bodyRequest: { g: value },
-                        endpoint: 'add_metal'
-                    })
-                        .then((response) => {
-                            console.log(response);
-                            deposited = true;
-                        })
-                        .catch(console.error);
-                    break;
-                case "#glass":
-                    putMaterialQuantity({
-                        id: stationId,
-                        bodyRequest: { g: value },
-                        endpoint: 'add_glass'
-                    })
-                        .then((response) => {
-                            console.log(response);
-                            deposited = true;
-                        })
-                        .catch(console.error);
-                    break;
-                case "#paper":
-                    putMaterialQuantity({
-                        id: stationId,
-                        bodyRequest: { g: value },
-                        endpoint: 'add_paper'
-                    })
-                        .then((response) => {
-                            console.log(response);
-                            deposited = true;
-                        })
-                        .catch(console.error);
-                    break;
-                case "#electronic":
-                    putMaterialQuantity({
-                        id: stationId,
-                        bodyRequest: { g: value },
-                        endpoint: 'add_electronic'
-                    })
-                        .then((response) => {
-                            console.log(response);
-                            deposited = true;
-                        })
-                        .catch(console.error);
-                    break;
-            }
-        }
-    };
+  var button = document.querySelector(`${boxId} button`);
+  button.onclick = () => {
+    var input = document.querySelector(`${boxId} input[name='quantity']`);
+    var value = parseInt(input.value);
+    if (value && stationId) {
+      var material = boxId.replace('#', '');
+      putMaterialQuantity({
+        id: stationId,
+        bodyRequest: { g: value },
+        endpoint: `add_${material}`,
+      })
+        .then((response) => {
+          console.log(response);
+          getStationData(stationId).then((station) => {
+            document.querySelector(
+              `${boxId} p.lead`
+            ).textContent = `Total: ${station[material]}`;
+          });
+        })
+        .catch(console.error);
+    }
+  };
 });
+
+if (stationId) {
+  getStationData(stationId).then((station) => {
+    document.querySelector('h1.display-4').textContent = station.name;
+    document.querySelector('p#desc').textContent = ``;
+    boxesIds.forEach((boxId) => {
+      var material = boxId.replace('#', '');
+      document.querySelector(
+        `${boxId} p.lead`
+      ).textContent = `Total: ${station[material]}`;
+    });
+  });
+}
