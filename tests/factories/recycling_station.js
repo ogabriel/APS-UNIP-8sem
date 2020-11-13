1c2433ea34103beb59ac1566ffafdfe5460419a0
Breno Lipi <43282619+brlipi@users.noreply.github.com>
2020-11-13 10:19:10 -0300

    Changing factory test to reflect POST changes
    Co-authored-by: ogabriel <gabrieloliver8991@gmail.com>

--- tests/factories/recycling_station.js
+++ tests/factories/recycling_station.js
@@ -7,10 +7,7 @@ const numberOptions = { min: 1, max: 1000000 };
 
 const defaultOptions = {
   name: faker.name.findName(),
-  localization: {
-    type: 'Point',
-    coordinates: [faker.address.latitude(), faker.address.longitude()],
-  },
+  coordinates: [faker.address.latitude(), faker.address.longitude()],
   plastic: faker.random.number(numberOptions),
   metal: faker.random.number(numberOptions),
   glass: faker.random.number(numberOptions),
