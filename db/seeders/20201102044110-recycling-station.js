183a2be774b5c17d0b0712fcde5b60aa12832cea
Gabriel Oliveira <gabrieloliver8991@gmail.com>
2020-11-15 17:50:06 -0300

    Add bigger seeders (#31)

--- db/seeders/20201102044110-recycling-station.js
+++ db/seeders/20201102044110-recycling-station.js
@@ -3,17 +3,17 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const usersQuery = await queryInterface.sequelize.query(
-      'SELECT u.id FROM "Users" u ORDER BY u.id LIMIT 2'
+      'SELECT u.id FROM "Users" u ORDER BY u.id LIMIT 5'
     );
 
     const users = usersQuery[0];
 
     return queryInterface.bulkInsert('RecyclingStations', [
       {
-        name: 'Recicloptero',
+        name: 'Reciclagem R5',
         localization: Sequelize.fn(
           'ST_GeomFromText',
-          'POINT(-46.635906 -23.554604)'
+          'POINT(-46.631906 -23.554604)'
         ),
         plastic: 200,
         metal: 350,
@@ -25,17 +25,73 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        name: 'R3 reciclagem',
+        name: 'Sucata de metal',
         localization: Sequelize.fn(
           'ST_GeomFromText',
-          'POINT(-46.633706 -23.554604)'
+          'POINT(-46.633906 -23.554804)'
+        ),
+        metal: 10,
+        UserId: users[1].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Reciclagem Só plastico',
+        localization: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(-46.634811 -23.556504)'
         ),
         plastic: 1080,
-        metal: 2160,
-        glass: 360,
-        paper: 180,
-        electronic: 720,
         UserId: users[1].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Sucata de eletrônicos',
+        localization: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(-46.635512 -23.557310)'
+        ),
+        paper: 1420,
+        electronic: 130,
+        UserId: users[1].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Recicla Papel',
+        localization: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(-46.635303 -23.558904)'
+        ),
+        paper: 1420,
+        UserId: users[1].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'Posto de reciclagem de vidro',
+        localization: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(-46.637909 -23.555705)'
+        ),
+        glass: 700,
+        UserId: users[1].id,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: 'UNIP Paraiso - reciclagem',
+        localization: Sequelize.fn(
+          'ST_GeomFromText',
+          'POINT(-46.6347724 -23.573701)'
+        ),
+        plastic: 1000,
+        metal: 1000,
+        glass: 1000,
+        paper: 1000,
+        electronic: 1000,
+        UserId: users[3].id,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -43,10 +99,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete(
-      'RecyclingStations',
-      [{ name: ['Recicloptero', 'R3 reciclagem'] }],
-      {}
-    );
+    return queryInterface.bulkDelete('RecyclingStations', [{}], {});
   },
 };
